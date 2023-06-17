659cc201abf6aa369941da84efaf6af71d5c5484
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-17 11:53:37 -0400

    Config file installation/packages
    - Basic config generated files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+
+export default {
+    plugins:
+    [
+        react()
+    ],
+    root: 'src/',
+    publicDir: "../public/",
+    base: './',
+    server:
+    {
+        host: true,
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
