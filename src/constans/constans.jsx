3cd6c17743e9103c7749e746f3759d47136cf5d4
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-23 13:01:27 -0400

    Create constans.jsx
    - Static information that will be retrieved for the projects component

---
+++ src/constans/constans.jsx
@@ -0,0 +1,93 @@
+import IGN from '../images/IGN.png'
+import Wedding from '../images/wedding.png'
+import Hexigon from '../images/Frame_2.png'
+import Playline from '../images/playline.png'
+// import Wedding from '../images/'
+// import Wedding from '../images/'
+
+export const works = [
+    {
+      name: "Hexigon",
+      description:
+        "An AI driven platform that allowed users to generate websites through prompts and descriptions",
+        tags: [
+          {
+            name: "react",
+            color: "text-blue-500",
+          },
+          {
+            name: "restapi",
+            color: "text-green-500",
+          },
+          {
+            name: "scss",
+            color: "text-pink-500",
+          },
+        ],
+      image: Hexigon,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Playline",
+      description:
+        "A online fantasy league site that allowed users to views live games, create teams, and generate tournaments to bet money",
+      tags: [
+        {
+          name: "react",
+          color: "text-blue-500",
+        },
+        {
+          name: "restapi",
+          color: "text-green-500",
+        },
+        {
+          name: "scss",
+          color: "text-pink-500",
+        },
+      ],
+      image: Playline,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "IGN Clone",
+      description:
+        "A video game news websites that regularly updates, providing sort features, images and detailed descriptions",
+        tags: [
+          {
+            name: "react",
+            color: "text-blue-500",
+          },
+          {
+            name: "restapi",
+            color: "text-green-500",
+          },
+          {
+            name: "scss",
+            color: "text-pink-500",
+          },
+        ],
+      image: IGN,
+      source_code_link: "https://github.com/",
+    },
+    {
+        name: "Wedding Site Mock Up",
+        description:
+            "A fully functioning wedding mock up site with an original design",
+            tags: [
+              {
+                name: "react",
+                color: "text-blue-500",
+              },
+              {
+                name: "restapi",
+                color: "text-green-500",
+              },
+              {
+                name: "scss",
+                color: "text-pink-500",
+              },
+            ],
+        image: Wedding,
+        source_code_link: "https://github.com/",
+      },
+  ];
