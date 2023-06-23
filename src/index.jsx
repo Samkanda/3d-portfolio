02fb29b9c88cbbc2115eaef0695d6bc0b87ac86b
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-23 12:24:19 -0400

    Update index.jsx
    -Implemented Canvas and Experience Component
    - Appears on Load

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.scss'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
@@ -6,12 +6,13 @@ import {About} from './components/About'
 import { Projects } from './components/Projects'
 import Embed from './Embed'
 
+
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
+    <h1 className=' absolute top 0 left-0 z-10 text-white cursor-pointer'>Expand</h1>
     {/* <Embed/> */}
-    {/* <Projects/> */}
     <Canvas
         camera={ {
             fov: 45,
