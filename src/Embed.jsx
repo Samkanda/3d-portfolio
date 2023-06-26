85ca8b093c56a0270efb10456a42aaaabcf8ff51
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-26 11:56:32 -0400

    Update Embed.jsx
    - Implemented react draggable funtionality
    - Passed setOpen status to the Window and Dock

--- src/Embed.jsx
+++ src/Embed.jsx
@@ -2,17 +2,23 @@ import { useCallback, useState } from 'react'
 import Navbar from "./components/Navbar"
 import { Dock } from "./components/Dock"
 import Window from "./components/Window"
-import { Hero } from "./components/Hero"
-import { About } from "./components/About"
-import { Projects } from "./components/Projects"
+import Draggable from "react-draggable";
 
 export default function Embed() {
-
+  const [open, setOpen] = useState(false)
+console.log(open)
   return (
     <>
-    <div className="EmbedContainer text-white h-full">
-    
-    </div>
+      <Navbar/>
+      <Draggable bounds=".EmbedContainer ">
+      <div className="EmbedContainer mt-10  text-white h-full">
+       
+        {open ?  <Window setOpen={setOpen}/>: ""}
+
+      </div>
+      </Draggable>
+      
+      <Dock setOpen={setOpen}/>
     </>
   )
 }
