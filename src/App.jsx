e85cb04a50218f324abf7e7e4a7cdffc68179704
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-26 11:54:26 -0400

    Update App.jsx
    - Transition from laptop view to website view using button functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useState } from 'react'
-import ReactDOM from 'react-dom'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import Embed from './Embed'
@@ -9,9 +8,13 @@ export default function App() {
 
   return (
     <>
-      {/* <h1 className=' absolute top 0 left-0 z-10 text-white cursor-pointer' onClick={() =>setLaptop(false)}>Expand</h1> */}
+      {laptop ? 
 
-      {/* {laptop ? ( */}
+      <h1 className=' font-bold border-4 p-2 border-sky-100 text-3xl absolute top-3 left-3 z-10 text-white cursor-pointer' 
+      onClick={() =>setLaptop(false)}>Expand</h1>
+    : ""
+    }
+      {laptop ? (
         <Canvas
           camera={{
             fov: 45,
@@ -20,11 +23,11 @@ export default function App() {
             position: [-3, 1.5, 4]
           }}
         >
-          {/* <Experience /> */}
+          <Experience />
         </Canvas>
-      {/* ) : (
+      ) : (
         <Embed />
-      )} */}
+      )}
     </>
   )
 }
