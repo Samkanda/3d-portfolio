1defca615dffc63e4d62180a81a326fd4074e7bc
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-06-26 11:51:56 -0400

    Update Hero.jsx
    - Revamped Hero section style design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,23 +5,43 @@ import {motion} from "framer-motion"
 export const Hero = () => {
   return (
     <section className='relative w-full mx-auto'>
-        <div className='  max-w-7xl mx-auto flex flex-row items-start gap-5'>
-            <div className='flex flex-col justify-center items-center mt-5'>
-                <div className='w-5 h-5 rounded-full bg-[#915EFF'/>
-                <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+       <h1 className=' font-mono text-6xl mb-2'>Intro</h1>
+       <div className=' w-40 h-2 rounded-lg bg-[#915EFF] '></div>
+        <p className='mt-4'>
+          I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development 
+          and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs.
+          My job is to build your website so that it is functional and user-friendly but at the same time 
+          attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. 
+          My aim is to bring across your message and identity in the most creative way. I created web design for many famous 
+          brand companies.
+        </p>
+        <div className=' mt-10 '> 
+          <h1 className=' text-xl font-bold'>What I'm Doing</h1>
+          <div className='flex mt-5 justify-between gap-4'>
+            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+            bg-gradient-to-r  text-white'>
+              <h1 className=' font-bold text-xl mb-2'>Web Design</h1>
+              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
             </div>
-            <div className=''>
-                <h1 className='text-white text-4xl font-mono font-bold' >
-                    Hi, I'm <span className='text-[#915EFF]'> Navpreet </span>
-                </h1>
-                <p className=' mt-2  text-xl text-white font-mono'>
-                    I developer 3D visuals, user interfaces and web applications
-                </p>
+            <div className='p-5 bg-[#273138] shadow-2xl shadow-black rounded-xl  text-white'>
+            <h1 className=' font-bold text-xl mb-2'>Web Development</h1>
+              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
             </div>
-        </div>
+            </div>
+            <div className='flex mt-5 justify-between gap-4'>
 
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-      </div>
+            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+            bg-gradient-to-r  text-white'>
+              <h1 className=' font-bold text-xl mb-2'>Mobile Apps</h1>
+              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+            </div>
+            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+            bg-gradient-to-r  text-white'>
+              <h1 className=' font-bold text-xl mb-2'>3D Animation</h1>
+              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+            </div>
+          </div>
+        </div>
     </section>
   )
 }
