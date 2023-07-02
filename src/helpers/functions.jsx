4ab905fbf528e505182f03ae92afff91372cb342
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-02 02:43:40 -0400

    Update functions.jsx
    - Implemented a resize function for the window

--- src/helpers/functions.jsx
+++ src/helpers/functions.jsx
@@ -5,3 +5,13 @@ export const openWindow = () => {
     console.log("test")
   return( <Window/>)
 }
+
+export const onResize = (container) => {
+  console.log("test")
+  if (container.current) {
+    const isFullScreen = container.current.style.width === '100vw';
+    container.current.style.width = isFullScreen ? '50vw' : '100vw';
+    container.current.style.height = isFullScreen ? '50%' : '100%';
+
+  }
+}
