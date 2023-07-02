599addd0bebb6d68cf3c5baefc85adbb5f23e283
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-02 02:45:39 -0400

    Create Modal.jsx
    - modal for widget display

---
+++ src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Modal = ({data}) => {
+  return (
+    <div className='sPanel sPanelNotifications sidepanel scrollable active'>
+    <div className='switch'>
+        Hexigon-Website builder
+    </div>
+    <div>Modalv</div>
+
+    </div>
+  )
+}
