0104cfc45fddc160d5d923094f5e78705d97a711
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-05 14:24:56 -0400

    Update Navbar.jsx
    - Passed prop to open and close modal
    - slight css changes to icon sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { SlMagnifier } from "react-icons/sl";
 
 
 
-const Navbar = () => {
+const Navbar = ({setModalOpen, modalOpen}) => {
     let newDate = new Date()
     let day = newDate.getDate();
     let month = newDate.getMonth() + 1;
@@ -16,7 +16,7 @@ const Navbar = () => {
     <nav className= "w-full flex h-10 px-2 items-center fixed top-0 hstack justify-between z-20 text-sm text-white bg-gray-700/10 backdrop-blur-2xl shadow transition ">
      <div className=' inline-flex items-center  '>
      <div className=' items-center space-x-1 h-6 cursor-default  inline-flex px-2'>
-        <span className=' text-2xl sm:text-3xl  space-x-1 h-8 px-1 cursor-default rounded inline-flex 
+        <span className=' text-2xl  space-x-1 h-8 px-1 cursor-default rounded inline-flex 
         hover:bg-gray-100/30 hover:dark:bg-gray-400/40 items-center '>
             <AiFillApple/>
         </span>
@@ -29,9 +29,9 @@ const Navbar = () => {
         <div className='hidden sm:inline-flex text-base'>100%</div>
         <div className='hidden sm:inline-flex  text-xl pl-4'>     <BsBatteryFull/></div>
         <div className='text-xl pl-4'>     <AiOutlineWifi/></div>
-        <div className='hidden sm:inline-flex  text-xl pl-4'>     <SlMagnifier/></div>
+        <div className='hidden sm:inline-flex  text-lg pl-4'>     <SlMagnifier/></div>
         <div className='text-xl pl-4'>     <BsFillHddStackFill/></div>
-        <div className='text-base pl-4'> Sun Jun {day} 10:00 PM </div>
+        <div className='cursor-pointer text-base px-3 ml-2 rounded-md hover:bg-gray-100/30 hover:dark:bg-gray-400/40' onClick={() => setModalOpen(!modalOpen)}> Sun Jun {day} 10:00 PM </div>
      </div>
     </nav>
   )
