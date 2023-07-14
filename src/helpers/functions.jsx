137fec6f47e89cd96ebc5e5af91864723fdf7917
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-13 23:34:56 -0400

    Update functions.jsx
    - changed from 100 to 95

--- src/helpers/functions.jsx
+++ src/helpers/functions.jsx
@@ -10,7 +10,7 @@ export const onResize = (container) => {
   if (container.current) {
     const isFullScreen = container.current.style.width === '100vw';
     container.current.style.width = isFullScreen ? '50vw' : '100vw';
-    container.current.style.height = isFullScreen ? '50%' : '100%';
+    container.current.style.height = isFullScreen ? '50%' : '95%';
 
   }
 }
