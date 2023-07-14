8ac565c37b930f303665cec84a42905e90008611
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-13 23:35:36 -0400

    Css changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import {motion} from "framer-motion"
 export const Hero = () => {
   return (
     <section className='relative w-full mx-auto'>
-       <h1 className=' font-mono text-6xl mb-2'>Intro</h1>
-       <div className=' w-40 h-2 rounded-lg bg-[#915EFF] '></div>
-        <p className='mt-4'>
+       <h1 className=' font-mono text-xl font-bold text-gray-400 mb-2'>Introduction</h1>
+       <div className=' w-36 h-2 rounded-lg bg-[#262626] '></div>
+        <p className='mt-4 text-gray-200'>
         Hello, I'm a Senior Web Developer with expertise in AI-powered web development, frontend development, and user experience. 
         Throughout my career, I have contributed to various projects and gained experience working with cutting-edge technologies such as Next.js, 
         React, Grapes.js, and Directus. Currently, I am working as a Senior Web Developer at Hexigon Company, where I led the development of a 
@@ -15,32 +15,29 @@ export const Hero = () => {
         developing scalable web applications, integrating drag-and-drop page builders, leveraging headless CMS solutions, and implementing AI 
         and machine learning features.
         </p>
-        <div className=' mt-10 '> 
-          <h1 className=' text-xl font-bold'>What I'm Doing</h1>
-          <div className='flex mt-5 justify-between gap-4'>
-            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+        <h1 className=' mt-10 text-xl font-bold'>What I'm Doing</h1>
+
+        <div className=' mt-5 gap-4 grid grid-cols-2 grid-rows-2'> 
+            <div className='p-5 bg-[#262626]  shadow-2xl shadow-black rounded-xl 
             bg-gradient-to-r  text-white'>
               <h1 className=' font-bold text-xl mb-2'>Web Design</h1>
-              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+              <p>Creating design mockups for mobile and desktop applications</p>
             </div>
-            <div className='p-5 bg-[#273138] shadow-2xl shadow-black rounded-xl  text-white'>
+            <div className='p-5 bg-[#262626] shadow-2xl shadow-black rounded-xl  text-white'>
             <h1 className=' font-bold text-xl mb-2'>Web Development</h1>
-              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+              <p>Using the latest technologies to create web applications</p>
             </div>
-            </div>
-            <div className='flex mt-5 justify-between gap-4'>
 
-            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+            <div className='p-5 bg-[#262626]  shadow-2xl shadow-black rounded-xl 
             bg-gradient-to-r  text-white'>
               <h1 className=' font-bold text-xl mb-2'>Mobile Apps</h1>
-              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+              <p>Using the latest technologies to create web applications</p>
             </div>
-            <div className='p-5 bg-[#273138]  shadow-2xl shadow-black rounded-xl 
+            <div className='p-5 bg-[#262626]  shadow-2xl shadow-black rounded-xl 
             bg-gradient-to-r  text-white'>
               <h1 className=' font-bold text-xl mb-2'>3D Animation</h1>
-              <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web developm</p>
+              <p>Implementing 3D models through the use of blender and three.js</p>
             </div>
-          </div>
         </div>
     </section>
   )
--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -5,23 +5,22 @@ import { Projects } from './Projects';
 import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 import {onResize} from '../helpers/functions'
 import {BsCodeSlash, BsListTask, BsBriefcase} from "react-icons/bs";
+// import {MdArrowBackIosNew, MdArrowForwardIos} from "react-icons/Md";
 
 const Window = ({setOpen}) => {
   const [tab, setTab] = useState("Intro");
   const container = useRef()
   const [isDragging, setIsDragging] = useState(true)
 
-
   return (
-    
     <Draggable
     axis="both"
     handle=".handle"
     defaultPosition={{x: 0, y: 30}}
-    position={!isDragging? { x: 0, y: 30 } : undefined}
+    position={!isDragging? { x: 0, y: -15 } : undefined}
     scale={1}
     bounds=".EmbedContainer"
-    // onStart={handleStart}
+    // onStart={position={ x: 0, y: 0 }}
     // onDrag={handleDrag}
     // onStop={handleStop}
     >
@@ -32,9 +31,21 @@ const Window = ({setOpen}) => {
         <div className="dot bg-red-500" onTouchStart={(e) =>  setOpen(false)}  onClick={()=> setOpen(false)}></div>
         <div className="dot bg-yellow-300"></div>
         <div className="dot bg-green-500" onTouchStart={(e) => (onResize(container),setIsDragging(!isDragging))}  onClick={() => (onResize(container),setIsDragging(!isDragging)) }></div>
+       <div className=' inline-block'>
+        {/* <div className='flex gap-4 ml-32'>
+        <MdArrowBackIosNew/>
+        <MdArrowForwardIos/>
+        <h1>Macintosh HD -- Introduciton</h1>
+        </div> */}
+        
+      </div> 
     </div>
-        <div className='  h-full flex flex-col sm:flex-row'>
-          <div className='left bg-[#22303C] text-gray-100  min-w-[10rem] h-48 sm:h-full  '>
+        <div className='h-full flex flex-col sm:flex-row'>
+          <div className='left bg-[#262626] text-sm text-gray-400 min-w-[10rem] h-48 sm:h-full'>
+          <h1 className='sm:flex text-sm mt-2 hidden items-center opacity-80 px-4' onTouchStart={(e) =>  setTab("Intro")} onClick={() => setTab("Intro")}>
+          Favorites
+          </h1>
+          <div className='ml-2'>
             <h1 className='flex  items-center  gap-2 interaction cursor-pointer px-4 py-4 hover:font-bold' onTouchStart={(e) =>  setTab("Intro")} onClick={() => setTab("Intro")}>
               <BsCodeSlash/> Introduction
                </h1>
@@ -44,8 +55,12 @@ const Window = ({setOpen}) => {
             <h1 className='flex items-center gap-2 interaction cursor-pointer mx-4 my-4 hover:font-bold' onTouchStart={(e) =>  setTab("Projects")} onClick={() => setTab("Projects")}> 
               <BsBriefcase/> Projects
             </h1>
+            {/* <h1 className='flex items-center gap-2 interaction cursor-pointer mx-4 py-4 hover:font-bold' onTouchStart={(e) =>  setTab("Contact")} onClick={() => setTab("Contact")}> 
+              <BsListTask/>Contact
+            </h1> */}
           </div>
-          <div className='right interaction pb-20 sm:pb-10  p-4 bg-[#1B1E1F] w-full h-full overflow-auto '>
+          </div>
+          <div className='right interaction pb-20 sm:pb-10  p-4 bg-[#1C1C1C] w-full h-full overflow-auto '>
             {tab === "Intro" && <Hero />} 
             {tab === "About" && <About />} 
             {tab === "Projects" && <Projects />} 
@@ -53,8 +68,6 @@ const Window = ({setOpen}) => {
         </div>
     </div> 
     </Draggable>
-   
-   
   )
 }
 
