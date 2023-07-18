12ba802a3bae45c042b743e35685d2e89cbeb630
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-07-18 16:43:50 -0400

    Deleted unnecessary divs redundant code

--- src/Embed.jsx
+++ src/Embed.jsx
@@ -3,30 +3,35 @@ import Navbar from "./components/Navbar"
 import { Dock } from "./components/Dock"
 import Window from "./components/Window"
 import Draggable from "react-draggable";
-import { Camera } from './components/Camera';
+import { Resume } from './components/Resume';
 import { Modal } from './components/Modal';
+import folder from './icons/folder.svg'
 
 export default function Embed() {
   const [open, setOpen] = useState(false)
   const [modalOpen, setModalOpen] = useState(false)
+  const [pdfOpen, setPdfOpen] = useState(false)
+
   return (
     <>
       <Navbar setModalOpen={setModalOpen} modalOpen={modalOpen}/>
-
       <div className="EmbedContainer text-white h-full">
-      <Draggable cancel={".interaction"} bounds={{left: 0, top: 0, right: 0, bottom: 0}} >
-        <div className='h-full w-full flex justify-center items-center absolute top-8'>        
+        <div className='h-full w-full flex justify-center items-center absolute top-10'>        
+        <div  onClick={() => setPdfOpen(!pdfOpen)} className='folder absoulte top-4 right-10 flex flex-col  items-center justify-center text-white interaction z-0'>
+        <img  className='' src={folder}/>
+        <p>Resume</p>
+      </div>
           {open ?  <Window setOpen={setOpen} />: ""}
+          {pdfOpen ? <Resume setOpen={setPdfOpen}/>: ""}
+          
+
         </div>
-      </Draggable>
-      {/* <Draggable cancel={".interaction"} bounds={{left: 0, top: 30, right: 0, bottom: 0}}>
-      <Camera/>
-      </Draggable> */}
+   
       {modalOpen ?  <Modal/> : ""}
     
 
       </div>
-      
+
       <Dock setOpen={setOpen} open={open}/>
     </>
   )
--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -4,8 +4,9 @@ import { About } from './About';
 import { Projects } from './Projects';
 import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 import {onResize} from '../helpers/functions'
-import {BsCodeSlash, BsListTask, BsBriefcase} from "react-icons/bs";
-// import {MdArrowBackIosNew, MdArrowForwardIos} from "react-icons/Md";
+import {BsFillTagFill, BsCodeSlash, BsListTask, BsBriefcase} from "react-icons/bs";
+import {MdViewColumn, MdArrowBackIosNew, MdArrowForwardIos, MdGridView, MdIosShare, MdFormatListBulleted, MdApps} from "react-icons/md";
+import { SlMagnifier } from "react-icons/sl"; 
 
 const Window = ({setOpen}) => {
   const [tab, setTab] = useState("Intro");
@@ -16,7 +17,8 @@ const Window = ({setOpen}) => {
     <Draggable
     axis="both"
     handle=".handle"
-    defaultPosition={{x: 0, y: 30}}
+    cancel={".interaction"}
+    defaultPosition={{x: 0, y: 30}} 
     position={!isDragging? { x: 0, y: -15 } : undefined}
     scale={1}
     bounds=".EmbedContainer"
@@ -25,20 +27,12 @@ const Window = ({setOpen}) => {
     // onStop={handleStop}
     >
     <div ref={container} className=' overflow-hidden h-full w-4/5 sm:h-[34rem] resize 
-    pb-10 bg-slate-600 rounded-t-lg max-w-full max-h-full'>
+    pb-10 bg-slate-600 rounded-t-lg max-w-full max-h-full md:min-w-[45rem] z-20'>
     <div  className=" handle outer p-2 pl-4 dark opacity-90 w-full rounded-[7px_7px_0px_0px];
     background: #b4b4b4">
         <div className="dot bg-red-500" onTouchStart={(e) =>  setOpen(false)}  onClick={()=> setOpen(false)}></div>
         <div className="dot bg-yellow-300"></div>
-        <div className="dot bg-green-500" onTouchStart={(e) => (onResize(container),setIsDragging(!isDragging))}  onClick={() => (onResize(container),setIsDragging(!isDragging)) }></div>
-       <div className=' inline-block'>
-        {/* <div className='flex gap-4 ml-32'>
-        <MdArrowBackIosNew/>
-        <MdArrowForwardIos/>
-        <h1>Macintosh HD -- Introduciton</h1>
-        </div> */}
-        
-      </div> 
+        <div className="dot bg-green-500 mr-2" onTouchStart={(e) => (onResize(container),setIsDragging(!isDragging))}  onClick={() => (onResize(container),setIsDragging(!isDragging)) }></div>
     </div>
         <div className='h-full flex flex-col sm:flex-row'>
           <div className='left bg-[#262626] text-sm text-gray-400 min-w-[10rem] h-48 sm:h-full'>
@@ -61,6 +55,20 @@ const Window = ({setOpen}) => {
           </div>
           </div>
           <div className='right interaction pb-20 sm:pb-10  p-4 bg-[#1C1C1C] w-full h-full overflow-auto '>
+          <div className='hidden  sm:inline-flex absolute top-3 item'>
+        <div className='flex gap-6 text-gray-400  items-center'>
+        <MdArrowBackIosNew/>
+        <MdArrowForwardIos/>
+        <h1 className=' font-bold'>Macintosh HD</h1>
+        </div>
+       
+        
+      </div> 
+      <div className=' hidden items-center sm:flex gap-4 absolute right-8 top-4 text-gray-400 '>
+        <div className='flex text-xl gap-4 mr-7'><MdGridView/> <MdIosShare/> <MdFormatListBulleted/></div>
+        <div className='flex text-xl gap-4 mr-7'><BsFillTagFill/> <MdApps/> <MdViewColumn/></div>
+        <SlMagnifier/>
+      </div>
             {tab === "Intro" && <Hero />} 
             {tab === "About" && <About />} 
             {tab === "Projects" && <Projects />} 
